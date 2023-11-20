7dcc0af854fea5b06c2733cc3e60d013d5a0b991
Louis Tremblay Thibault <tremblaythibaultl@gmail.com>
2023-11-20 15:34:50 +0100

    feat(): add step-by-step blind rotation

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,10 +1,12 @@
+use std::time::Instant;
+
 use methods::{BLIND_ROTATE_ELF, BLIND_ROTATE_ID};
 use risc0_zkvm::{default_prover, ExecutorEnv};
 use ttfhe::{
-    ggsw::compute_bsk,
-    glwe::keygen,
+    ggsw::{compute_bsk, BootstrappingKey},
+    glwe::{keygen, GlweCiphertext},
     lwe::{lwe_keygen, LweCiphertext},
-    utils::{decode_bootstrapped, encode},
+    utils::encode,
 };
 
 fn main() {
@@ -12,24 +14,38 @@ fn main() {
     let sk2 = keygen();
     let bsk = compute_bsk(&sk1, &sk2); // list of encryptions under `sk2` of the bits of `sk1`.
 
-    // let ksk = compute_ksk(&sk2.recode(), &sk1); // list of encryptions under `sk1` of the bits of `sk2`.
-
     let c = LweCiphertext::encrypt(encode(2), &sk1).modswitch(); // "noisy" ciphertext that will be bootstrapped
 
-    let env = ExecutorEnv::builder()
-        .write(&c)
-        .unwrap()
-        .write(&bsk)
-        .unwrap()
-        .build()
-        .unwrap();
+    step_by_step_blind_rotation(&c, &bsk)
+}
 
-    let prover = default_prover();
+fn step_by_step_blind_rotation(c: &LweCiphertext, bsk: &BootstrappingKey) {
+    let mut c_prime = GlweCiphertext::trivial_encrypt_lut_poly();
 
-    let receipt = prover.prove_elf(env, BLIND_ROTATE_ELF).unwrap();
+    c_prime.body = c_prime.body.multiply_by_monomial((2048 - c.body) as usize);
 
-    receipt.verify(BLIND_ROTATE_ID).unwrap();
-    let res_ct: LweCiphertext = receipt.journal.decode().unwrap();
-    let res_pt = decode_bootstrapped(res_ct.decrypt(&sk1));
-    println!("Computed bootstrapping and got plaintext {}", res_pt);
+    for i in 0..bsk.len() {
+        let now = Instant::now();
+
+        let env = ExecutorEnv::builder()
+            .write(&bsk[i])
+            .unwrap()
+            .write(&c_prime)
+            .unwrap()
+            .write(&c_prime.rotate(c.mask[i]))
+            .unwrap()
+            .build()
+            .unwrap();
+
+        let prover = default_prover();
+
+        let receipt = prover.prove_elf(env, BLIND_ROTATE_ELF).unwrap();
+
+        receipt.verify(BLIND_ROTATE_ID).unwrap();
+        c_prime = receipt.journal.decode().unwrap();
+        println!(
+            "Computed bootstrapping step number {i} in {}",
+            now.elapsed().as_secs()
+        );
+    }
 }
--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -3,21 +3,19 @@
 // #![no_std]  // std support is experimental
 
 use risc0_zkvm::guest::env;
-use ttfhe::{ggsw::BootstrappingKey, glwe::GlweCiphertext, lwe::LweCiphertext};
+use ttfhe::{
+    ggsw::{cmux, GgswCiphertext},
+    glwe::GlweCiphertext,
+};
 risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
-    // bincode can serialize `bsk` into a blob that weighs 39.9MB on disk.
-    // This `env::read()` call doesn't seem to stop - memory is allocated until the process goes OOM with risc0 v0.17.0.
-    let (c, bsk): (LweCiphertext, BootstrappingKey) = env::read();
+    println!("cycle count: {}", env::get_cycle_count());
+    // Reading the three components required to perform one step of the blind rotation (one CMUX)
+    let (bsk_i, c_prime, c_prime_rotated): (GgswCiphertext, GlweCiphertext, GlweCiphertext) =
+        env::read();
 
-    let lut = GlweCiphertext::trivial_encrypt_lut_poly();
+    let res = cmux(&bsk_i, &c_prime, &c_prime_rotated);
 
-    // `blind_rotate` is a quite heavy computation that takes ~2s to perform on a M2 MBP.
-    // Maybe this is why the process is running OOM?
-    let blind_rotated_lut = lut.blind_rotate(c, &bsk);
-
-    let res_ct = blind_rotated_lut.sample_extract();
-
-    env::commit(&res_ct);
+    env::commit(&res);
 }
