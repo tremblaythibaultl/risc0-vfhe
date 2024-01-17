5e0ad6cbc34dcf79bcad1c340d41f8a71398a60b
Louis Tremblay Thibault <tremblaythibaultl@gmail.com>
2024-01-17 10:54:56 -0500

    add framework for private input benchmarking

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,20 +1,25 @@
 use methods::{BLIND_ROTATE_ELF, BLIND_ROTATE_ID};
 use risc0_zkvm::{default_prover, ExecutorEnv};
 use ttfhe::{
-    ggsw::compute_bsk,
-    glwe::keygen,
-    lwe::{lwe_keygen, LweCiphertext},
+    ggsw::{compute_bsk, BootstrappingKey},
+    glwe::{keygen, SecretKey},
+    lwe::{lwe_keygen, LweCiphertext, LweSecretKey},
     utils::{decode_bootstrapped, encode},
 };
 
 fn main() {
-    let sk1 = lwe_keygen();
-    let sk2 = keygen();
-    let bsk = compute_bsk(&sk1, &sk2); // list of encryptions under `sk2` of the bits of `sk1`.
+    // let sk1 = lwe_keygen();
+    let sk1: LweSecretKey = bincode::deserialize(&std::fs::read("../data/sk1").unwrap()).unwrap();
+    // let sk2 = keygen();
+    let sk2: SecretKey = bincode::deserialize(&std::fs::read("../data/sk2").unwrap()).unwrap();
+    // let bsk = compute_bsk(&sk1, &sk2); // list of encryptions under `sk2` of the bits of `sk1`.
+    let bsk: BootstrappingKey =
+        bincode::deserialize(&std::fs::read("../data/bsk").unwrap()).unwrap();
 
     // let ksk = compute_ksk(&sk2.recode(), &sk1); // list of encryptions under `sk1` of the bits of `sk2`.
 
-    let c = LweCiphertext::encrypt(encode(2), &sk1).modswitch(); // "noisy" ciphertext that will be bootstrapped
+    // let c = LweCiphertext::encrypt(encode(2), &sk1).modswitch(); // "noisy" ciphertext that will be bootstrapped
+    let c: LweCiphertext = bincode::deserialize(&std::fs::read("../data/ct").unwrap()).unwrap();
 
     let env = ExecutorEnv::builder()
         .write(&c)
@@ -32,4 +37,23 @@ fn main() {
     let res_ct: LweCiphertext = receipt.journal.decode().unwrap();
     let res_pt = decode_bootstrapped(res_ct.decrypt(&sk1));
     println!("Computed bootstrapping and got plaintext {}", res_pt);
+}
+
+#[test]
+fn generate_bsk() {
+    let sk1 = lwe_keygen();
+    let sk2 = keygen();
+    let bsk = compute_bsk(&sk1, &sk2); // list of encryptions under `sk2` of the bits of `sk1`.
+
+    let ct = LweCiphertext::encrypt(encode(2), &sk1).modswitch();
+
+    let sk1_ser = bincode::serialize(&sk1).unwrap();
+    let sk2_ser = bincode::serialize(&sk2).unwrap();
+    let bsk_ser = bincode::serialize(&bsk).unwrap();
+    let ct_ser = bincode::serialize(&ct).unwrap();
+
+    std::fs::write("../data/sk1", sk1_ser).unwrap();
+    std::fs::write("../data/sk2", sk2_ser).unwrap();
+    std::fs::write("../data/bsk", bsk_ser).unwrap();
+    std::fs::write("../data/ct", ct_ser).unwrap();
 }
--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -2,22 +2,39 @@
 // If you want to try std support, also update the guest Cargo.toml file
 // #![no_std]  // std support is experimental
 
+use include_bytes_aligned::include_bytes_aligned;
 use risc0_zkvm::guest::env;
-use ttfhe::{ggsw::BootstrappingKey, glwe::GlweCiphertext, lwe::LweCiphertext};
+use ttfhe::{
+    ggsw::{BootstrappingKey, GgswCiphertext},
+    glwe::GlweCiphertext,
+    lwe::LweCiphertext,
+    LWE_DIM,
+};
 risc0_zkvm::guest::entry!(main);
 
 pub fn main() {
+    let start = env::get_cycle_count();
     // bincode can serialize `bsk` into a blob that weighs 39.9MB on disk.
     // This `env::read()` call doesn't seem to stop - memory is allocated until the process goes OOM with risc0 v0.17.0.
-    let (c, bsk): (LweCiphertext, BootstrappingKey) = env::read();
+    // let (c, bsk): (LweCiphertext, BootstrappingKey) = env::read();
+
+    static BSK_BYTES: &[u8] = include_bytes_aligned!(8, "../../../data/bsk");
+    // static CT_BYTES: &[u8] = include_bytes_aligned!(8, "../../../data/ct");
+
+    let bsk = unsafe { std::mem::transmute::<&u8, &[GgswCiphertext; LWE_DIM]>(&BSK_BYTES[0]) };
+
+    // let ct: LweCiphertext = unsafe { std::mem::transmute::<&u8, LweCiphertext>(&CT_BYTES[0]) };
 
     let lut = GlweCiphertext::trivial_encrypt_lut_poly();
 
     // `blind_rotate` is a quite heavy computation that takes ~2s to perform on a M2 MBP.
     // Maybe this is why the process is running OOM?
-    let blind_rotated_lut = lut.blind_rotate(c, &bsk);
+    let blind_rotated_lut = lut.blind_rotate(c, &bsk.to_vec());
 
-    let res_ct = blind_rotated_lut.sample_extract();
+    // let res_ct = blind_rotated_lut.sample_extract();
 
-    env::commit(&res_ct);
+    // env::commit(&res_ct);
+
+    let end = env::get_cycle_count();
+    eprintln!("start: {}, end: {}", start, end);
 }
