dce10bf25f68f232c52eafac16390cb48ed54e43
tremblaythibaultl <tremblaythibaultl@gmail.com>
2025-07-08 13:15:32 +0200

    step-by-step, bump version

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
-# Verifiable fully homomorphic encryption
+# Verifiable fully homomorphic encryption with Risc0
 
-This repository hosts a proof of concept for verifiable fully homomorphic encryption. 
+This repository hosts a proof of concept for verifiable fully homomorphic encryption.
 
 The aim of this project is to prove and verify the correct execution of a single TFHE bootstrapping.
+
+## Instructions
+
+### Requirements
+Risc0 zkVM (and all of its subdependencies): see installation procedure [here](https://dev.risczero.com/api/zkvm/install).
+
+### Code structure
+The project is divided in a guest (code to be executed in the zkVM) and a host (orchestrator for proving and verifying the guest code).
+This follows paradigms introduced by Risc0. See [Risc0](https://dev.risczero.com/api/zkvm/) for more information on the project structure enforced by the zkVM.
+
+The code relies on the [ttfhe](https://github.com/tremblaythibaultl/ttfhe) library, a simple (and thus zkVM-compatible) Rust implementation of the [TFHE](https://eprint.iacr.org/2018/421) cryptosystem. 
+
+### Usage
+1. Build the program:
+```bash
+$ cd program 
+$ cargo prove build
+```
+2. Execute the program:
+```bash
+$ cd ../script 
+$ RUST_LOG=info cargo run --release -- --execute
+```
--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -1,37 +1,17 @@
-#![no_main]
-// If you want to try std support, also update the guest Cargo.toml file
-// #![no_std]  // std support is experimental
-
 use risc0_zkvm::guest::env;
-use ttfhe::{ggsw::BootstrappingKey, glwe::GlweCiphertext, lwe::LweCiphertext};
-risc0_zkvm::guest::entry!(main);
+use ttfhe::{
+    ggsw::{cmux, GgswCiphertext},
+    glwe::GlweCiphertext,
+};
 
-pub fn main() {
-    let start = env::cycle_count();
-    // bincode can serialize `bsk` into a blob that weighs 39.9MB on disk.
-    // This `env::read()` call doesn't seem to stop - memory is allocated until the process goes OOM with risc0 v0.17.0.
-    let (c, bsk): (LweCiphertext, BootstrappingKey) = env::read();
-    
-    let after_load = env::cycle_count();
+fn main() {
+    // read the input
+    let (bsk_i, c_prime, c_prime_rotated): (GgswCiphertext, GlweCiphertext, GlweCiphertext) =
+        env::read();
 
-    let lut = GlweCiphertext::trivial_encrypt_lut_poly();
+    // compute a cmux operation (one step of the blind rotation)
+    let res = cmux(&bsk_i, &c_prime, &c_prime_rotated);
 
-    // `blind_rotate` is a quite heavy computation that takes ~2s to perform on a M2 MBP.
-    // Maybe this is why the process is running OOM?
-    let blind_rotated_lut = lut.blind_rotate(c, &bsk);
-
-    let after_br = env::cycle_count();
-
-    let res_ct = blind_rotated_lut.sample_extract();
-
-    let after_se = env::cycle_count();
-
-    env::commit(&res_ct);
-
-    let end = env::cycle_count();
-
-    eprintln!(
-        "start: {}\nafter load (before br): {}\nafter br: {}\nafter se: {}\nend: {}",
-        start, after_load, after_br, after_se, end
-    );
+    // commit to the result
+    env::commit(&res);
 }
